c26aa1c64375f847f84cfb21f960b9c89698fa7f
LMS Developer <developer@lms.com>
2025-10-08 19:29:44 +0300

    Update admin dashboard config for correct repository deployment

--- frontend/admin-dashboard/vite.config.js
+++ frontend/admin-dashboard/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/algoriza-internship2025-FS99-BE-byway/',
+  base: '/algoriza-internship2025-FS99-FE-byway-admin/',
   server: {
     port: 3001,
     proxy: {
